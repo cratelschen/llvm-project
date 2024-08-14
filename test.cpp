2407e9bc1c2b180cc1e1e9bae00be114ea596970
chenshuang <chenshuang@softsafe-tech.com>
2024-08-14 15:09:00 +0800

    add comment: 分析 cc1_main

--- test.cpp
+++ test.cpp
@@ -1,1 +1,26 @@
-int f() { return 1; }
+#include <iostream>
+
+using namespace std;
+class A {
+public:
+  A() { std::cout << "ctor" << std::endl; }
+
+  A(const A &a) { std::cout << "copy ctor" << std::endl; }
+
+  A(A &&a) noexcept { std::cout << "move ctor" << std::endl; }
+};
+
+A getA() {
+  int i = 1;
+  if (i) {
+    return A();
+  }
+  return A();
+}
+
+int main(int argc, const char **argv) {
+
+  A act(getA());
+
+  return 0;
+}
