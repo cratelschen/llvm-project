28ad4c7add93fe3dcb22366c0a3cd529ead32c97
chenshuang <chenshuang@softsafe-tech.com>
2024-09-11 17:11:07 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,30 +1,11 @@
-#include "./x_add/add.h"
-#include <iostream>
+extern int gm;
+int gi = 1;
 
-using namespace std;
-class A {
-public:
-  A() { std::cout << "ctor" << std::endl; }
-
-  A(const A &a) { std::cout << "copy ctor" << std::endl; }
-
-  A(A &&a) noexcept { std::cout << "move ctor" << std::endl; }
-};
-
-A getA() {
-  int i = 1;
-  if (i) {
-    return A();
-  }
-  return A();
-}
+int add(int i, int j) { return i + j; }
 
 int main(int argc, const char **argv) {
-
-  int i = add(1, 2);
-  std::cout << i << std::endl;
-
-  A act(getA());
-
+  int i = 2;
+  int b = add(gi, gm);
+  i = b;
   return 0;
 }
